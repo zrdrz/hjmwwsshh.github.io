064d3006d256a392bda31b7ddc8ce407fa82f232
huangjm <admin@ykecf.cn>
2019-03-29 17:54:14 +0800

    update Mist_dmgcal.html Mist_dmgcal_data.js;增加半伤和过穿计算公式

--- Mist_dmgcal_data.js
+++ Mist_dmgcal_data.js
@@ -1,6 +1,15 @@
 //存放数据
 //若要扩充weapon类型,armor类型或是ship类型都十分方便
 //
+//增加新公式的步骤:
+//1.确定要用到的变量,增加到global{}里面
+//2.在result{}中写好存放结果的object
+//3.若要新增武器效率系数,则写入到Dict_weapon_data[weapontype].efficiency里面
+//4.若新增的系数需要展示,则需要在Dict_var_element_bind_inputbox_readonly里绑定html页面上对应的元素id
+//5.写好新公式的function,并加入到Dict_funtions内,便可调用
+//
+//修改shiptype和weapontype,直接对Dict_shiptype_data和Dict_weapon_data进行操作即可
+//
 //全局变量
 var global = { 
     shiptype: "",
@@ -15,8 +24,20 @@ var global = {
     equipmentefficiency: 0,
     critdmg_base: 0,
     additional_crit: 0,
+    overpene_dmg: 0,
+    moderate_dmg: 0,
 };
 var result = { //计算结果存放处
+    singledmg_overpene: {
+        vsLightArmor: 0.00,
+        vsMediumArmor: 0.00,
+        vsHeavyArmor: 0.00
+    },
+    singledmg_moderate: {
+        vsLightArmor: 0.00,
+        vsMediumArmor: 0.00,
+        vsHeavyArmor: 0.00
+    },
     singledmg: {
         vsLightArmor: 0.00,
         vsMediumArmor: 0.00,
@@ -35,12 +56,15 @@ var result = { //计算结果存放处
 };
 //
 //存放html页面对应元素的id,key值要与global中的key值相同
-const Dict_var_element_bind_inputbox_readonly = { //部分与Dict_weapon_data[weapontype].efficiency绑定
+const Dict_var_element_bind_inputbox_readonly = { 
+//部分key值与Dict_weapon_data[weapontype].efficiency的key值绑定,value为页面的elementid
     vsLightArmor: "WeaponvsLightArmor",
     vsMediumArmor: "WeaponvsMediumArmor",
     vsHeavyArmor: "WeaponvsHeavyArmor",
     equipmentefficiency: "equipmentefficiency",
-    critdmg_base: "critdmg_base"
+    critdmg_base: "critdmg_base",
+    moderate_dmg: "moderate_dmg",
+    overpene_dmg: "overpene_dmg",
 };
 const Dict_var_element_bind_inputbox_readwrite = {
     firepower: "firepower",
@@ -62,11 +86,13 @@ const Dict_barrels = {
 const Dict_weapon_data = { //weapon数据
     AP_ExBig: {
         name: "超大AP",  //显示名
-        efficiency: {
-            vsLightArmor: 0.35, //各项效率
+        efficiency: { //各项效率
+            vsLightArmor: 0.35, 
             vsMediumArmor: 1.00,
             vsHeavyArmor: 0.75,
             critdmg_base: 2.00, //暴击系数也放在这里面了
+            moderate_dmg: 0.50, //半伤系数
+            overpene_dmg: 0.25, //过穿伤害系数
         },
         barrels: [ "2","3" ] //可用的联装数
     },
@@ -77,6 +103,8 @@ const Dict_weapon_data = { //weapon数据
             vsMediumArmor: 0.75,
             vsHeavyArmor: 0.30,
             critdmg_base: 2.00,
+            moderate_dmg: 0.50,
+            overpene_dmg: 0.25,
         },
         barrels: [ "2","3" ] 
     },
@@ -87,6 +115,8 @@ const Dict_weapon_data = { //weapon数据
             vsMediumArmor: 0.50,
             vsHeavyArmor: 0.25,
             critdmg_base: 2.00,
+            moderate_dmg: 0.50,
+            overpene_dmg: 0.25,
         },
         barrels: [ "1","2","3" ] 
     },
@@ -97,6 +127,8 @@ const Dict_weapon_data = { //weapon数据
             vsMediumArmor: 0.35,
             vsHeavyArmor: 0.25,
             critdmg_base: 2.00,
+            moderate_dmg: 0.50,
+            overpene_dmg: 0.25,
         },
         barrels: [ "1","2" ] 
     },
@@ -107,6 +139,8 @@ const Dict_weapon_data = { //weapon数据
             vsMediumArmor: 1.00,
             vsHeavyArmor: 0.50,
             critdmg_base: 1.50,
+            moderate_dmg: 0.67,
+            overpene_dmg: 0.25,
         },
         barrels: [ "2","3" ] 
     },
@@ -117,6 +151,8 @@ const Dict_weapon_data = { //weapon数据
             vsMediumArmor: 0.75,
             vsHeavyArmor: 0.40,
             critdmg_base: 1.50,
+            moderate_dmg: 0.67,
+            overpene_dmg: 0.25,
         },
         barrels: [ "2","3" ] 
     },
@@ -127,6 +163,8 @@ const Dict_weapon_data = { //weapon数据
             vsMediumArmor: 0.50,
             vsHeavyArmor: 0.25,
             critdmg_base: 1.50,
+            moderate_dmg: 0.67,
+            overpene_dmg: 0.25,
         },
         barrels: [ "1","2","3" ] 
     },
@@ -137,6 +175,8 @@ const Dict_weapon_data = { //weapon数据
             vsMediumArmor: 0.35,
             vsHeavyArmor: 0.25,
             critdmg_base: 1.50,
+            moderate_dmg: 0.67,
+            overpene_dmg: 0.25,
         },
         barrels: [ "1","2" ] 
     },
@@ -147,6 +187,8 @@ const Dict_weapon_data = { //weapon数据
             vsMediumArmor: 0.75,
             vsHeavyArmor: 1.50,
             critdmg_base: 2.00,
+            moderate_dmg: 0.50,
+            overpene_dmg: 0.25,
         },
         barrels: [ "1","2","3" ] 
     },
@@ -157,6 +199,8 @@ const Dict_weapon_data = { //weapon数据
             vsMediumArmor: 0.75,
             vsHeavyArmor: 0.75,
             critdmg_base: 1.50,
+            moderate_dmg: 0.67,
+            overpene_dmg: 0.25,
         },
         barrels: [ "3" ] 
     },
@@ -167,6 +211,8 @@ const Dict_weapon_data = { //weapon数据
             vsMediumArmor: 0.75,
             vsHeavyArmor: 1.00,
             critdmg_base: 2.00,
+            moderate_dmg: 0.50,
+            overpene_dmg: 0.25,
         },
         barrels: [ "3" ] 
     }
@@ -241,15 +287,40 @@ const Dict_shiptype_data = {  //ship数据
 //存放要调用的计算的函数名:
 Dict_funtions = {
     init_calinfo: "dmgcal_info()",
+    cal_singledmgoverpene: "dmgcal_singledmg_overpene()",
+    cal_singledmgmoderate: "dmgcal_singledmg_moderate()",
     cal_singledmg: "dmgcal_singledmg()",
     cal_singledmgcrit: "dmgcal_singdmg_crit()",
+    printline: "printSeparateline()",
     cal_dpm: "dmgcal_dpm()",
     
+};
+function printSeparateline() {
+    var str = "------" + '\n'
+    return str;
 };
 function dmgcal_info() {
     var str = "舰船类型:" + Dict_shiptype_data[global.shiptype].name + '\t' + "武器类型:" + Dict_weapon_data[global.weapontype].name + '\n' + "面板总火力:" + global.firepower + '\t' + "火力补正:" + global.fp_correction + '\n' + "装填时间:" + global.reload_time + "s" + '\t' + "联装数:" + global.barrels + '\n'; 
     str = str + '-------------------------------------------------------' + '\n';
     str = str + '\t\t' + "vs轻甲" + '\t\t' + "vs中甲" + '\t\t' + "vs重甲" + '\n';
+    return str;
+};
+function dmgcal_singledmg_overpene(){
+    var str = "单发过穿" + '\t';
+    for ( varname in result.singledmg_overpene ){ 
+        result.singledmg_overpene[varname] = Math.ceil(global.firepower * global.fp_correction * global[varname] * global.equipmentefficiency * global.overpene_dmg * global.reload_time / global.barrels / 5 / 100);
+        str = str + result.singledmg_overpene[varname] + '\t\t';
+    };
+    str = str + '\n';
+    return str;
+};
+function dmgcal_singledmg_moderate(){
+    var str = "单发半伤" + '\t';
+    for ( varname in result.singledmg_moderate ){ 
+        result.singledmg_moderate[varname] = Math.ceil(global.firepower * global.fp_correction * global[varname] * global.equipmentefficiency * global.moderate_dmg * global.reload_time / global.barrels / 5 / 100);
+        str = str + result.singledmg_moderate[varname] + '\t\t';
+    };
+    str = str + '\n';
     return str;
 };
 function dmgcal_singledmg(){
